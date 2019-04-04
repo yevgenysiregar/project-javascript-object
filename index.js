f1770f05273d9ad7f073701430defd83dd5b1683
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-04 18:00:00 +0700

    Add index.js and add text inside

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log({ name: "Yevgeny", age: 20, superPower: false });
+console.log({
+  name: "Yevgeny",
+  age: 20,
+  superPower: false
+});
+
+const human = { name: "Yevgeny", age: 20, superPower: false };
+
+console.log(human);
+console.log(human["name"]);
+console.log(human.age);
+console.log(human.superPower);
+
+alert(human);
+alert(human["name"]);
+alert(human.age);
+alert(human.superPower);
